421e4c19053b46a1b5c90afe959863ce11b8df32
nnnikkk777 <ushakovv.nick@gmail.com>
2021-12-03 22:55:50 +0200

    H/W #6-7

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,52 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Layout from "@/components/Layout.vue";
+
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/tasks',
+    redirect: "/tasks",
     children: [
       {
-        path: '/activity',
-        name: 'activity',
-        component: () => import(/* webpackChunkName: "activity" */ '../views/activity.vue')
+        path: "/activity",
+        name: "activity",
+        component: () => import(/* webpackChunkName: "activity" */ "../views/activity.vue")
       },
       {
-        path: '/tasks',
-        name: 'tasks',
-        component: () => import(/* webpackChunkName: "tasks" */ '../views/tasks.vue')
+        path: "/tasks",
+        name: "tasks",
+        component: () => import(/* webpackChunkName: "tasks" */ "../views/tasks.vue")
       },
       {
-        path: '/kanban',
-        name: 'kanban',
-        component: () => import(/* webpackChunkName: "kanban" */ '../views/kanban.vue')
+        path: "/kanban",
+        name: "kanban",
+        component: () => import(/* webpackChunkName: "kanban" */ "../views/kanban.vue")
       },
       {
-        path: '/files',
-        name: 'files',
-        component: () => import(/* webpackChunkName: "files" */ '../views/files.vue')
+        path: "/files",
+        name: "files",
+        component: () => import(/* webpackChunkName: "files" */ "../views/files.vue")
       },
       {
-        path: '/calendar',
-        name: 'calendar',
-        component: () => import(/* webpackChunkName: "calendar" */ '../views/calendar.vue')
+        path: "/calendar",
+        name: "calendar",
+        component: () => import(/* webpackChunkName: "calendar" */ "../views/calendar.vue")
       }
     ]
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import(/* webpackChunkName: "404" */ "../views/404.vue")
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404'
   }
-
 ]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  linkActiveClass: 'active',
+  linkActiveClass: "active",
   routes
-})
-export default router
+});
+export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.general
+  p(class="NotFound")
+    | Page not found
+  router-link(class="Back" to="/")
+    | Back to home page
+</template>
+
+<script lang="ts">
+export default {
+  name: "404"
+};
+</script>
+
+<style scoped>
+.general {
+  min-height: 100vh;
+  min-width: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.NotFound {
+  font-family: Helvetica;
+  font-size: 60px;
+  color: #42b983;
+}
+.Back {
+  font-size: 40px;
+  font-family: Helvetica;
+  color: #9B9B9B;
+}
+</style>
